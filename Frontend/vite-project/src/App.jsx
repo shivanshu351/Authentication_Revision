fcd962ac2e2a2e62f380f9698582ff105c918d9c
Shivanshu Singh <shivanshusingh71@gmail.com>
2024-09-13 00:59:36 +0530

    "routes sorted"

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,17 +1,18 @@
-import Signup from "../components/Signup"
-import Login from "../components/Login"
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Signup from "../components/Signup";
+import Login from "../components/Login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-
- return (
+  return (
     <>
+      <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup/>} />
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
